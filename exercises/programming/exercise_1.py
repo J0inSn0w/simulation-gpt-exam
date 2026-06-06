1899f1db2c879adc59923d6fb94de0cd50a060f2
jesusito <jesuslomas2@gmail.com>
2026-06-05 21:30:44 -0700

    Programming ejercicio 1

---
+++ exercises/programming/exercise_1.py
@@ -0,0 +1,12 @@
+"""
+
+Nos piden saber cuanto imprime el codigo esto es muy sencillo si hacemos una sumatoria de Gauss 5(5 + 1) / 2 = 15
+
+"""
+
+x = 0
+
+for i in range(1, 6):
+    x += i
+
+print(x)
