ba6c6079a0ee0388115402e6efa0897edc47864e
jesusito <jesuslomas2@gmail.com>
2026-06-05 23:40:38 -0700

    programming ejercicio 13

---
+++ exercises/programming/exercise_13.py
@@ -0,0 +1,17 @@
+"""
+Algoritmo BuscarElemento
+
+Entrada: lista, elemento
+
+Para i ← 0 hasta longitud(lista) - 1 hacer
+    Si lista[i] = elemento entonces
+        Mostrar "Elemento encontrado en la posición ", i
+        Terminar
+    Fin Si
+Fin Para
+
+Mostrar "Elemento no encontrado"
+
+Fin Algoritmo
+
+"""
