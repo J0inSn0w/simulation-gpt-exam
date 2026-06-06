ab53e7c4a826d7a3ee16fdf0aecc0feeca573197
jesusito <jesuslomas2@gmail.com>
2026-06-05 22:47:53 -0700

    Programming ejercicio 8

---
+++ exercises/programming/exercise_8.py
@@ -0,0 +1,19 @@
+"""
+
+Algoritmo EncontrarMaximo
+
+Entrada: lista de números
+
+maximo ← lista[0]
+
+Para cada numero en lista hacer
+    Si numero > maximo entonces
+        maximo ← numero
+    Fin Si
+Fin Para
+
+Mostrar maximo
+
+Fin Algoritmo
+
+"""
